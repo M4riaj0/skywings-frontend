cba91a78d424f077effa9763e252269703fccea5
Andres Palacio <andres.palacio@utp.edu.co>
2024-11-28 09:36:21 -0500

    Created checkin services

---
+++ src/services/checkin.ts
@@ -0,0 +1,31 @@
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;;
+
+export const checkin = async (flightCode: string, passengerDNI: string) => {
+  try {
+    const response = await fetch(`${BACKEND_URL}/checkin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ flightCode, passengerDNI}),
+    });
+    return response.json();
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const changeSeat = async (flightCode: string, passengerDNI: string, seatNumber: string) => {
+  try {
+    const response = await fetch(`${BACKEND_URL}/checkin/change-seat`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ flightCode, passengerDNI, seatNumber}),
+    });
+    return response.json();
+  } catch (error) {
+    throw error
+  }
+}
