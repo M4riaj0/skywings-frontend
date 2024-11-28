355003dcea54f13adf261718a955a604bb92dc97
Andres Palacio <andres.palacio@utp.edu.co>
2024-11-28 09:37:54 -0500

    Correct patch method

--- src/services/checkin.ts
+++ src/services/checkin.ts
@@ -3,7 +3,7 @@ const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;;
 export const checkin = async (flightCode: string, passengerDNI: string) => {
   try {
     const response = await fetch(`${BACKEND_URL}/checkin`, {
-      method: 'POST',
+      method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
       },
@@ -18,7 +18,7 @@ export const checkin = async (flightCode: string, passengerDNI: string) => {
 export const changeSeat = async (flightCode: string, passengerDNI: string, seatNumber: string) => {
   try {
     const response = await fetch(`${BACKEND_URL}/checkin/change-seat`, {
-      method: 'POST',
+      method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
       },
