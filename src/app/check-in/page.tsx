2a16348b2c756e034fd7de743c35adacb38e65be
m4r14j0 <mariajose@esvyda.com>
2024-12-05 22:45:18 -0500

    se añadío una card para la vista

--- src/app/check-in/page.tsx
+++ src/app/check-in/page.tsx
@@ -6,13 +6,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { checkin, getTicket } from "@/services/checkin";
-import {
-  Box,
-  Snackbar,
-  Alert,
-  TextField,
-  Button,
-  Typography,
+import { 
+  Box, 
+  Snackbar, 
+  Alert, 
+  TextField, 
+  Button, 
+  Typography, 
+  Card, 
+  CardContent, 
+  CardActions 
 } from "@mui/material";
 import { getFlightSeats } from "@/services/flights";
 
@@ -28,14 +31,14 @@ export default function CheckIn() {
     flightCode: z
       .string()
       .min(6, {
-        message: "El nombre de usuario debe tener al menos 6 caracteres",
+        message: "El código de vuelo debe tener al menos 6 caracteres",
       })
       .max(12, {
-        message: "El nombre de usuario no puede tener más de 12 caracteres",
+        message: "El código de vuelo no puede tener más de 12 caracteres",
       })
       .regex(/^[a-zA-Z0-9_]+$/, {
         message:
-          "El nombre de usuario solo puede contener letras, números y guiones bajos",
+          "El código de vuelo solo puede contener letras, números y guiones bajos",
       }),
     passengerDni: z
       .string()
@@ -64,19 +67,14 @@ export default function CheckIn() {
   const onSubmit = handleSubmit(async (data) => {
     try {
       const response = await checkin(data.flightCode, data.passengerDni);
-      console.log("response active::", response);
-
       if (response.ok) {
         setSnackBarMessage("Check-in realizado correctamente.");
         setSnackBarSeverity("success");
       } else {
-        console.log("error:");
         setSnackBarMessage("Error al realizar el check-in.");
         setSnackBarSeverity("error");
       }
     } catch (err) {
-      console.log("response cattch::", err);
-      console.error("Error al realizar el check-in:", err);
       setSnackBarMessage("Error al realizar el check-in.");
       setSnackBarSeverity("error");
     } finally {
@@ -88,7 +86,9 @@ export default function CheckIn() {
     try {
       const { flightCode, passengerDni } = getValues();
       const ticket = await getTicket(flightCode, passengerDni);
+      console.log("ticket::::", ticket);
       const response = await getFlightSeats(flightCode);
+      console.log("response seats::::", response);
       if (response) {
         const query = new URLSearchParams({
           ...response,
@@ -104,14 +104,13 @@ export default function CheckIn() {
         setSnackBarSeverity("error");
       }
     } catch (err) {
-      console.error("Error al realizar el check-in:", err);
       setSnackBarMessage("Error al realizar el check-in.");
       setSnackBarSeverity("error");
     }
   };
 
   return (
-    <Box>
+    <Box sx={{ display: "flex", justifyContent: "center", mt: 8 }}>
       {/* Snackbar */}
       <Snackbar
         open={snackBarOpen}
@@ -139,65 +138,66 @@ export default function CheckIn() {
           {snackBarMessage}
         </Alert>
       </Snackbar>
-      <Typography variant="h4" className="text-center my-8">
-        Check-In
-      </Typography>
-      <form onSubmit={onSubmit} className="w-3/5 mx-auto my-16">
-        <Typography>
-          Realiza el check-in ingresando el código de vuelo y el DNI del
-          pasajero.
-        </Typography>
-        <Controller
-          name="flightCode"
-          control={control}
-          rules={{ required: "Este campo es obligatorio" }}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              label="Código de vuelo"
-              fullWidth
-              margin="dense"
-              error={!!errors.flightCode}
-              helperText={errors.flightCode ? errors.flightCode.message : ""}
+
+      {/* Card */}
+      <Card sx={{ maxWidth: 500, width: "100%", boxShadow: 3 }}>
+        <CardContent>
+          <Typography variant="h4" align="center" gutterBottom>
+            Check-In
+          </Typography>
+          <Typography align="center" paragraph>
+            Realiza el check-in ingresando el código de vuelo y el DNI del
+            pasajero.
+          </Typography>
+          <form onSubmit={onSubmit}>
+            <Controller
+              name="flightCode"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="Código de vuelo"
+                  fullWidth
+                  margin="normal"
+                  error={!!errors.flightCode}
+                  helperText={errors.flightCode?.message}
+                />
+              )}
             />
-          )}
-        />
-        <Controller
-          name="passengerDni"
-          control={control}
-          rules={{ required: "Este campo es obligatorio" }}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              label="DNI del pasajero"
-              fullWidth
-              margin="dense"
-              error={!!errors.passengerDni}
-              helperText={
-                errors.passengerDni ? errors.passengerDni.message : ""
-              }
+            <Controller
+              name="passengerDni"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="DNI del pasajero"
+                  fullWidth
+                  margin="normal"
+                  error={!!errors.passengerDni}
+                  helperText={errors.passengerDni?.message}
+                />
+              )}
             />
-          )}
-        />
-        <Box className="w-full flex justify-between my-4">
+          </form>
+        </CardContent>
+        <CardActions sx={{ justifyContent: "space-between", p: 2 }}>
           <Button
             type="submit"
             color="primary"
             variant="contained"
-            style={{ opacity: 1 }}
+            onClick={onSubmit}
           >
             Confirmar Check-In
           </Button>
           <Button
-            onClick={changeSeat}
             variant="contained"
             color="secondary"
-            style={{ opacity: 1 }}
+            onClick={changeSeat}
           >
             Cambiar asiento
           </Button>
-        </Box>
-      </form>
+        </CardActions>
+      </Card>
     </Box>
   );
 }
